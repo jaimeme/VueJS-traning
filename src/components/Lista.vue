60bbef9e51068d2d3c236d69278a6b10c87cbc4a
Jaime Ribeiro <jaimeribeiro99@gmail.com>
2020-05-02 18:37:22 -0300

    :art: Adding a new module

---
+++ src/components/Lista.vue
@@ -0,0 +1,20 @@
+<template>
+  <section>
+    <p></p>
+  </section>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      lista: [
+        { id: 1, nome: "Jaime" },
+        { id: 2, nome: "João" },
+        { id: 3, nome: "Jadson" }
+      ]
+    };
+  }
+};
+</script>
+<style scoped>
+</style>
