88221f41877c95dd1cc7ed11b3c1455340f755a6
Jaime Ribeiro <jaimeribeiro99@gmail.com>
2020-05-02 21:16:31 -0300

    :card_file_box: Adding data list ,template and img

--- src/components/Lista.vue
+++ src/components/Lista.vue
@@ -1,10 +1,19 @@
 <template>
   <section>
-    <p></p>
+    <br />
+    <p v-for="item in lista" :key="item.id">Nome do usuário {{item.id}}: {{item.nome}}</p>
+    <!--está sendo usado item.id ao invés do lsita.id por causa que está dentro da lógica v-for (foreach) -->
+    <img :src="importLogo()" />
   </section>
 </template>
 <script>
 export default {
+  methods: {
+    importLogo() {
+      return require("../assets/logo.png");
+      //feito uma importação de arquivo por método
+    }
+  },
   data() {
     return {
       lista: [
@@ -13,8 +22,14 @@ export default {
         { id: 3, nome: "Jadson" }
       ]
     };
-  }
+  },
+  name: "Lista"
 };
 </script>
 <style scoped>
+p {
+  color: #01a880; /* css aplicado somente a este template */
+}
 </style>
+
+
